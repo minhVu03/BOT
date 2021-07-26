bc4f93fc6246e540e0c447c0df0c5bc660d3e898
Minh Vu <58742927+minhVu03@users.noreply.github.com>
2021-07-26 19:33:40 +0000

    attachments and more events

--- main.py
+++ main.py
@@ -15,21 +15,41 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    #i think this is to prevent the bot from replying to itself
     if message.author == client.user:
-        return
+      return
 
     #if message starts with this
-    if message.content.startswith('hello') or message.content.startswith('$hi'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('hello'):
+      await message.channel.send('Hello!')
 
-    #if the whole message is this
+    #Show list of commands
+    if message.content == '$commands':
+      await message.channel.send(
+        '```$info \n$simp \n$thunder \n$technosimp```'
+      )
+
+    ################### COMMANDS #########################
     if message.content == '$info':
-        await message.channel.send(
-            "My name is Carrota and I am a bot <3"
-        )
+      await message.channel.send(
+          "My name is Carrota version 1.0 and I am a bot <3"
+      )
+    if message.content == '$simp':
+      await message.channel.send(
+          "https://i.pinimg.com/originals/e0/41/fa/e041fa5038a055ff62d51fdbcc15dbc9.jpg"
+      )
+    #send attatchment
+    if message.content == '$thunder':
+      await message.channel.send(file=discord.File('zenitsu.gif'))
+      await message.channel.send('BOOM! Flash warning')
 
+    if message.content == '$technosimp':
+      await message.channel.send(file=discord.File('minhanh.PNG'))
+      await message.channel.send('Technosimp confirmed')
+
+    ################## message contains ###################
     if 'simp' in message.content:
-      await message.channel.send('A friendly reminder to stop simping. We do not condone this behaviour :3')
+      await message.channel.send('A friendly reminder to limit the simping. It is not healthy :3')
 
 #Run the client on the server (channel id: right click on channel)
 keep_alive()
