e3d6052049c63b44caf697dfa7e48772165f81af
Minh <58742927+minhVu03@users.noreply.github.com>
2021-07-29 03:56:30 +0000

    SOS command added. wait for user input. read in lowercase

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ async def on_message(message):
     #Show list of commands
     if message.content == '$commands':
       await message.channel.send(
-        '```$info \n$simp \n$thunder \n$technosimp```'
+        '```$info \n$simp \n$thunder \n$technosimp \n$selfdestruct \n$SOS```'
       )
 
     ################### COMMANDS #########################
@@ -44,12 +44,39 @@ async def on_message(message):
       await message.channel.send('BOOM! Flash warning')
 
     if message.content == '$technosimp':
-      await message.channel.send(file=discord.File('minhanh.PNG'))
-      await message.channel.send('Technosimp confirmed')
+      await message.channel.send(file=discord.File('minhanh.jpeg'))
+      await message.channel.send('```Technosimp identified and confirmed```')
 
-    ################## message contains ###################
-    if 'simp' in message.content:
-      await message.channel.send('A friendly reminder to limit the simping. It is not healthy :3')
+    if message.content == '$selfdestruct':
+      await message.channel.send('Thats funnyyy >:)')
+
+    if message.content.lower() == '$sos':
+      await message.channel.send("Yikess. There's a rope that can save you! **Climb** it or **Perish**!")
+      # This will make sure that the response will only be registered if the following
+      # conditions are met:
+      def check(msg):
+          return msg.author == message.author and msg.channel == message.channel and \
+          msg.content.lower() in ["climb", "perish"]
+
+      msg = await client.wait_for("message", check=check)
+      if msg.content.lower() == "climb":
+          await message.channel.send("HAHA *SNIP* I cut the rope now you're dead too :3")
+      elif msg.content.lower() == "perish":
+          await message.channel.send("RIP :3")
+
+    #################### message contains #####################
+    if 'this is so sad' in message.content or 'This is so sad' in message.content:
+      await message.channel.send('https://music.youtube.com/watch?v=kJQP7kiw5Fk&list=RDAMVMkJQP7kiw5Fk')
+
+    if 'simp' in message.content or 'Simp' in message.content or 'SIMP' in message.content:
+      await message.channel.send('```A friendly reminder to limit the simping. It is not healthy :3```')
+
+    if 'eat' in message.content or 'Eat' in message.content or 'EAT' in message.content:
+      await message.channel.send(file=discord.File('delicious.jpg'))
+
+    if 'giyu' in message.content or 'Giyu' in message.content or 'GIYU' in message.content or 'tomioka' in message.content or 'Tomioka' in message.content or 'TOMIOKA' in message.content:
+      await message.channel.send(file=discord.File('tomioka.png'))
+      await message.channel.send('Tapioca?')
 
 #Run the client on the server (channel id: right click on channel)
 keep_alive()
