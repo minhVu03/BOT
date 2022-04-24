2a83d33bfe75cead8c0637cb6adb2bbe00b1fdc3
Minh <58742927+minhVu03@users.noreply.github.com>
2022-04-24 21:23:06 +0000

    updated april 2022

--- main.py
+++ main.py
@@ -5,13 +5,22 @@ import os
 
 #client (our bot)
 client = discord.Client()
-
+#----------------------------------------------------------
+#features to be added:
+#output random statements
+#make the bot have a personality
+#a dictionary: send link to word
+#----------------------------------------------------------
 #@client.event
 #async def on_ready():
     # DO STUFF...
     #general_channel = client.get_channel(841365211944714324)
     #wait to find the channel, then send a message when the bot comes online
     #await general_channel.send("Carrota hath cometh aliveth!!")
+
+@client.event
+async def on_member_join(member):
+    await member.send(file=discord.File('welcome.jpg'))
 
 @client.event
 async def on_message(message):
@@ -23,62 +32,98 @@ async def on_message(message):
     if message.content.startswith('hello'):
       await message.channel.send('Hello!')
 
+    #Dictionary
+    #word = ''
+    #if message.content.startwith == '$meaning' + word:
+    #  await message.channel.send('https://www.urbandictionary.com/define.php?term=' + word)
+
+    #ping pong 
+    if message.content == 'ping':
+      await message.channel.send('pong')
+
+		#meow
+    if message.content == 'meow':
+      await message.channel.send('meoww')
+
+		#lol
+    if message.content == 'is everett a girl':
+      await message.channel.send('oh yea 100%')
+
     #Show list of commands
     if message.content == '$commands':
       await message.channel.send(
-        '```$info \n$simp \n$thunder \n$technosimp \n$selfdestruct \n$SOS```'
+        '```$info \n$thunder \n$technosimp \n$selfdestruct \n$SOS \n$emotionalSupport \n$insomnia```'
       )
 
     ################### COMMANDS #########################
     if message.content == '$info':
       await message.channel.send(
-          "My name is Carrota version 1.0 and I am a bot <3\nI shall only ever obey Minh and noone else >:)"
-      )
-    if message.content == '$simp':
-      await message.channel.send(
-          "https://i.pinimg.com/originals/e0/41/fa/e041fa5038a055ff62d51fdbcc15dbc9.jpg"
+          "My name is Carrota version 1.0 and I am a bot <3\nI shall only ever obey minojuno and noone else >:)"
       )
     #send attatchment
     if message.content == '$thunder':
       await message.channel.send(file=discord.File('zenitsu.gif'))
-      await message.channel.send('BOOM! Flash warning')
 
     if message.content == '$technosimp':
-      await message.channel.send(file=discord.File('minhanh.jpeg'))
-      await message.channel.send('```Technosimp identified and confirmed```')
+      #await message.channel.send(file=discord.File('minhanh.jpeg'))
+      await message.channel.send('```Definitely Minh Anh```')
+
+		#emotional support
+    if message.content == '$emotionalSupport':
+      await message.channel.send('Carrota wants you to know that everything will be ok. Just pretend like nothing is on fire right now.')
+
+		#insomnia
+    if message.content == '$insomnia':
+      await message.channel.send('pff who needs sleep anyway, what a waste of time .-.')
 
     if message.content == '$selfdestruct':
-      await message.channel.send('Thats funnyyy >:)')
+      await message.channel.send('Warning: This action is destructive and cannot be reversed. **Confirm** self-destruct, or **Cancel**')
+      def check(msg):
+        return msg.author == message.author and msg.channel == message.channel and \
+        msg.content.lower() in ["confirm", "cancel"]
+      #wait for user input
+      msg = await client.wait_for("message", check=check)
+      if msg.content.lower() == "confirm":
+        await message.channel.send("Thats funnyyy >:) Do you think I'm dumb?")
+        def check(msg):
+          return msg.author == message.author and msg.channel == message.channel and \
+          msg.content.lower() in ["yes", "no"]
+        #wait for user input again
+        msg2 = await client.wait_for("message", check=check)
+        if msg2.content.lower() == "yes":
+          await message.channel.send("No u <3")
+        else:
+          await message.channel.send("awh thanks!")
+      elif msg.content.lower() == "cancel":
+        await message.channel.send("Self-destruct cancelled")
 
     if message.content.lower() == '$sos':
       await message.channel.send("Yikess. There's a rope that can save you! **Climb** it or **Perish**!")
       # This will make sure that the response will only be registered if the following
       # conditions are met:
       def check(msg):
-          return msg.author == message.author and msg.channel == message.channel and \
-          msg.content.lower() in ["climb", "perish"]
-
+        return msg.author == message.author and msg.channel == message.channel and \
+        msg.content.lower() in ["climb", "perish"]
+      #wait for user input
       msg = await client.wait_for("message", check=check)
       if msg.content.lower() == "climb":
-          await message.channel.send("HAHA *SNIP* I cut the rope now you're dead too :3")
+        await message.channel.send(file=discord.File('laugh.gif'))
+        await message.channel.send("HAHA *SNIP* I cut the rope now you're dead too :3")
       elif msg.content.lower() == "perish":
-          await message.channel.send("RIP :3")
+        await message.channel.send(file=discord.File('laugh.gif'))
+        await message.channel.send("RIP :3")
 
     #################### message contains #####################
-    if 'this is so sad' in message.content or 'This is so sad' in message.content:
-      await message.channel.send('https://music.youtube.com/watch?v=kJQP7kiw5Fk&list=RDAMVMkJQP7kiw5Fk')
+    if 'this is so sad' in message.content or 'This is so sad' in message.content or 'sad' in message.content or 'Sad' in message.content:
+      await message.channel.send('https://www.youtube.com/watch?v=kJQP7kiw5Fk')
 
     if 'simp' in message.content or 'Simp' in message.content or 'SIMP' in message.content:
       await message.channel.send('```A friendly reminder to limit the simping. It is not healthy :3```')
 
-    if 'eat' in message.content or 'Eat' in message.content or 'EAT' in message.content:
+    if 'food' in message.content or 'Food' in message.content or 'FOOD' in message.content:
       await message.channel.send(file=discord.File('delicious.jpg'))
 
-    if 'giyu' in message.content or 'Giyu' in message.content or 'GIYU' in message.content or 'tomioka' in message.content or 'Tomioka' in message.content or 'TOMIOKA' in message.content:
-      await message.channel.send(file=discord.File('tomioka.png'))
-      await message.channel.send('Tapioca?')
-
-#Run the client on the server (channel id: right click on channel)
+#Run the client on the server 
 keep_alive()
 TOKEN = os.environ.get("DISCORD_BOT_SECRET")
 client.run(TOKEN)
